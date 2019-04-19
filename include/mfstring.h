36f7c4f003cc8bb812b68bb8c3d2453a5bce91f8
Marek Fischer <marek5886@hotmail.com>
2019-04-19 23:00:55 +0200

    added atoi and isspace func

--- include/mfstring.h
+++ include/mfstring.h
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/06 19:27:14 by mfischer          #+#    #+#             */
-/*   Updated: 2019/04/15 17:44:38 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/04/19 22:57:20 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <boolean.h>
 
 char	*mf_itoa(int num);
 void	mf_itoa_s(int num, char *res);
@@ -25,5 +26,7 @@ void    mf_strclr(char *s);
 char    *mf_strdup(const char *s1);
 size_t  mf_strlen(const char *s);
 char    *mf_strjoin(char const *s1, char const *s2);
+int		mf_atoi(const char *str);
+t_bool	mf_isspace(const char c);
 
 #endif
