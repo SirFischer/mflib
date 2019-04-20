1eb62747a507b4d4c845e16118fbb62841c65233
Marek Fischer <marek5886@hotmail.com>
2019-04-20 10:02:36 +0200

    pleeease work, header conflicts

--- mflib.h
+++ mflib.h
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/30 23:44:39 by mfischer          #+#    #+#             */
-/*   Updated: 2019/04/19 21:27:54 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/04/20 10:02:06 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@
 ** ------------------------------ **
 */
 
-# include "include/boolean.h"
 # include "include/math_types.h"
 # include "include/mfmath.h"
 # include "include/mfstring.h"
@@ -31,5 +30,6 @@
 # include "include/mflist2.h"
 # include "include/mfmemory.h"
 # include "include/mfstack.h"
+# include "include/boolean.h"
 
 #endif
