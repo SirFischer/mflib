c3672339fa529c926d075358f6509cefc8b99124
Marek FISCHER <mfischer@e3r5p13.42.fr>
2019-06-06 16:06:34 +0200

    made res in matvec multiply independent for cleaner code

--- src/math/matvec_multiply.c
+++ src/math/matvec_multiply.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 23:24:22 by mfischer          #+#    #+#             */
-/*   Updated: 2019/04/13 14:51:52 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/06/06 16:06:03 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,49 +16,55 @@ void	mat4vec4_multiply(double mat[4][4], double vec[4], double res[4])
 {
 	int i;
 	int j;
+	double tmp[4];
 
 	i = -1;
-	vec4_init(res);
+	vec4_init(tmp);
 	while (++i < 4)
 	{
 		j = -1;
 		while (++j < 4)
 		{
-			res[i] += mat[i][j] * vec[j];
+			tmp[i] += mat[i][j] * vec[j];
 		}
 	}
+	vec4_copy(res, tmp);
 }
 
 void	mat3vec3_multiply(double mat[3][3], double vec[3], double res[3])
 {
 	int i;
 	int j;
+	double tmp[3];
 
 	i = -1;
-	vec3_init(res);
+	vec3_init(tmp);
 	while (++i < 3)
 	{
 		j = -1;
 		while (++j < 3)
 		{
-			res[i] += mat[i][j] * vec[j];
+			tmp[i] += mat[i][j] * vec[j];
 		}
 	}
+	vec3_copy(res, tmp);
 }
 
 void	mat2vec2_multiply(double mat[2][2], double vec[2], double res[2])
 {
 	int i;
 	int j;
+	double tmp[2];
 
 	i = -1;
-	vec2_init(res);
+	vec2_init(tmp);
 	while (++i < 2)
 	{
 		j = -1;
 		while (++j < 2)
 		{
-			res[i] += mat[i][j] * vec[j];
+			tmp[i] += mat[i][j] * vec[j];
 		}
 	}
+	vec2_copy(res, tmp);
 }
