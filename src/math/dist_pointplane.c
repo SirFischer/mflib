24ab29eb17c5cd44f07c59e7d37c20febb107703
Marek FISCHER <mfischer@e3r2p4.42.fr>
2019-06-11 16:34:46 +0200

    changed distfunc

--- src/math/dist_pointplane.c
+++ src/math/dist_pointplane.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 13:39:30 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/07 13:44:12 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/06/11 16:34:36 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 double		dist_pointplane(double plane_n[3], double plane_p[3], double point[3])
 {
-	return (vec3_dot(plane_n, point) - vec3_dot(plane_n, plane_p));
+	double tmp[3];
+	vec3vec3_substract(point, plane_p, tmp);
+	return (vec3_dot(tmp, plane_n));
 }
