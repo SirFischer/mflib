76f03227465bcc541d4eb96889ef4f59a489de32
Marek Fischer <marek5886@hotmail.com>
2019-07-05 23:46:47 +0200

    added to headers

--- mflib.h
+++ mflib.h
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/30 23:44:39 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/13 14:02:08 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/07/05 23:46:30 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@
 # include "include/mfstack.h"
 # include "include/boolean.h"
 # include "include/mfthreadpool.h"
+# include "include/mfsort.h"
 
 #endif
