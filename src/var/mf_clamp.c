45f4f0c1c4c177e5c0e1a707a8de09057356baeb
Marek Fischer <marek5886@hotmail.com>
2019-07-12 11:55:49 +0200

    optimized clamp functions

--- src/var/mf_clamp.c
+++ src/var/mf_clamp.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 23:42:34 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/12 23:51:56 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/07/12 11:55:29 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,12 @@
 
 int		mf_clamp_int(int value, int min, int max)
 {
-	if (value < min)
-		return (min);
-	if (value > max)
-		return (max);
-	return (value);
+	const int tmp = (value < min) ? min : value;
+	return ((tmp > max) ? max : tmp);
 }
 
 double	mf_clamp_double(double value, double min, double max)
 {
-	if (value < min)
-		return (min);
-	if (value > max)
-		return (max);
-	return (value);
+	const double tmp = (value < min) ? min : value;
+	return ((tmp > max) ? max : tmp);
 }
