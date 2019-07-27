994322a9a53b4f61890d81783b8a80ef3b9866ee
Marek FISCHER <mfischer@e1r12p22.42.fr>
2019-07-27 15:28:01 +0200

    fixed scale function' '

--- src/math/mat_scale.c
+++ src/math/mat_scale.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 22:33:25 by mfischer          #+#    #+#             */
-/*   Updated: 2019/04/13 14:51:17 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/07/27 15:25:46 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		mat4_scale(double mat[4][4], double x, double y, double z)
 	mat[0][0] *= x;
 	mat[1][1] *= y;
 	mat[2][2] *= z;
-	mat4mat4_multiply(mat, tmp, res);
+	mat4mat4_multiply(tmp, mat, res);
 	mat4_copy(mat, res);
 }
 
@@ -33,6 +33,6 @@ void		mat3_scale(double mat[3][3], double x, double y)
 	mat3_init(tmp);
 	mat[0][0] *= x;
 	mat[1][1] *= y;
-	mat3mat3_multiply(mat, tmp, res);
+	mat3mat3_multiply(tmp, mat, res);
 	mat3_copy(mat, res);
 }
