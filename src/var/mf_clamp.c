e802ead8a2b857867c81bd7755feb5d92e2d94e0
Marek FISCHER <mfischer@e1r11p21.42.fr>
2019-08-30 12:45:16 +0200

    fixed clamp function

--- src/var/mf_clamp.c
+++ src/var/mf_clamp.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 23:42:34 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/12 11:55:29 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/30 12:44:56 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 int		mf_clamp_int(int value, int min, int max)
 {
-	const int tmp;
+	const int tmp = (value < min) ? min : value;
 
-	tmp = (value < min) ? min : value;
 	return ((tmp > max) ? max : tmp);
 }
 
 double	mf_clamp_double(double value, double min, double max)
 {
-	const double tmp;
+	const double tmp = (value < min) ? min : value;
 
-	tmp = (value < min) ? min : value;
 	return ((tmp > max) ? max : tmp);
 }
