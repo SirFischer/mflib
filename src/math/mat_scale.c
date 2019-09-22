4b3c6fe43f2559f03756fc7e62d095ec67adb15d
ZzkilzZ <marek5886@hotmail.com>
2019-09-22 20:54:02 +0200

    made matrix functions follow glm style

--- src/math/mat_scale.c
+++ src/math/mat_scale.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 22:33:25 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/27 15:25:46 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/09/22 20:53:37 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_mat4d		mat4_scale(t_mat4d mat, double x, double y, double z)
 	mat.a[0][0] *= x;
 	mat.a[1][1] *= y;
 	mat.a[2][2] *= z;
-	return (mat4mat4_multiply(tmp, mat));
+	return (mat4mat4_multiply(mat, tmp));
 }
 
 t_mat3d		mat3_scale(t_mat3d mat, double x, double y)
@@ -30,5 +30,5 @@ t_mat3d		mat3_scale(t_mat3d mat, double x, double y)
 	mat3_init(&tmp);
 	mat.a[0][0] *= x;
 	mat.a[1][1] *= y;
-	return (mat3mat3_multiply(tmp, mat));
+	return (mat3mat3_multiply(mat, tmp));
 }
--- src/math/mat_translate.c
+++ src/math/mat_translate.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 22:26:49 by mfischer          #+#    #+#             */
-/*   Updated: 2019/04/13 14:51:23 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/09/22 20:51:27 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_mat4d	mat4_translate(t_mat4d mat, double x, double y, double z)
 	tmp.a[0][3] += x;
 	tmp.a[1][3] += y;
 	tmp.a[2][3] += z;
-	return (mat4mat4_multiply(tmp, mat));
+	return (mat4mat4_multiply(mat, tmp));
 }
 
 t_mat3d	mat3_translate(t_mat3d mat, double x, double y)
@@ -30,5 +30,5 @@ t_mat3d	mat3_translate(t_mat3d mat, double x, double y)
 	mat3_init(&tmp);
 	tmp.a[0][2] += x;
 	tmp.a[1][2] += y;
-	return (mat3mat3_multiply(tmp, mat));
+	return (mat3mat3_multiply(mat, tmp));
 }
